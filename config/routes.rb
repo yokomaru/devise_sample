0e69ecefd037ecebaf09af2ce6d955ec3bddcef7
yokomaru <cabochetoganlan@gmail.com>
2024-06-18 22:51:50 +0900

    add: letter_opener

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: 'homes#top'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
